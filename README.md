17100eca8fe8219f3fd42d10aaa65e4f7ba3b36e
Kenneth Duverge <Kenneth.Duverge1@gmail.com>
2018-04-15 12:14:51 -0400

    app setup with modules

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project Starter Code
+
+
+This Starter Project is just to save time in writing tedious code. It utilizes Express on the backend, and uses ejs for its templating engine. Route is set up in its folder pointing to index.js and its being required in the app.js .
+
+Run the following commands to get started
+
+>`npm install`
+
+
+Enjoy :)
---
+++ app.js
@@ -0,0 +1,53 @@
+const express           = require('express'),
+      bodyParser        = require('body-parser'),
+      methodOverride    = require('method-override'),
+      LocalStrategy     = require('passport-local'),
+      cookieParser      = require('cookie-parser'),
+      passport          = require('passport'),
+      mongoose          = require('mongoose'),
+      User              = require('./models/user.js'),
+      app               = express(),
+      path              = require('path'),
+      port              = process.env.PORT || 3000;
+
+const indexRoute = require('./routes/index.js');
+
+mongoose.Promise = global.Promise;
+
+const databaseUri = 'mongodb://kenny:kenny@ds149974.mlab.com:49974/not-instagram';
+
+mongoose.connect(databaseUri, { useMongoClient: true })
+      .then(() => console.log(`Database connected`))
+      .catch(err => console.log(`Database connection error: ${err.message}`));
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(require("express-session")({
+      secret: "This is not instagram",
+      resave: false,
+      saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+app.use("/", indexRoute);
+
+app.listen(port, (req, res) => console.log(`Server started on port ${port}...`));
